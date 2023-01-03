f89907de139a15b4e9ca7c56a6e823a117fccab0
asaad-tt <aasadshumon86@gmail.comm>
2023-01-03 08:59:17 +0600

    module local

---
+++ Lesson1/index.js
@@ -0,0 +1,6 @@
+const loli = require("./student");
+
+// console.log(loli.name());
+
+// console.log(loli.getAge());
+console.log(loli.hero);
---
+++ Lesson1/student.js
@@ -0,0 +1,19 @@
+const getName = () => {
+  return "kalo bador";
+};
+
+const getAge = () => {
+  return "25";
+};
+
+const hero = "zayed";
+
+/* exports.name = getName;
+exports.getAge = getAge;
+exports.film = hero; */
+
+module.exports = {
+  getName,
+  getAge,
+  hero,
+};
